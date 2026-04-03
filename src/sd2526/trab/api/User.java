86c168dd5df09160b6a3b81e5f1a82b84e0c4709
Mateus <m.albergaria@campus.fct.unl.pt>
2026-04-03 20:09:43 +0100

    alteracao nos servers, passamos todos os 2

--- src/sd2526/trab/api/User.java
+++ src/sd2526/trab/api/User.java
@@ -3,11 +3,26 @@ package sd2526.trab.api;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 /**
  * Represents a user in the system.
  */
 @Entity
 public class User {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(pwd, user.pwd) && Objects.equals(domain, user.domain) && Objects.equals(displayName, user.displayName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, pwd, domain, displayName);
+    }
+
     @Id
     private String name;	
     private String pwd;
